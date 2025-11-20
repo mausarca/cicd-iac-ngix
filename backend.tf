73a72ceaca2b3e077a2edf1bcd82de79ef7662ba
mausarca <mauricio.sarria@sotecmo.com.co>
2025-11-19 20:17:54 -0500

    IaC Pruebas - 191125:2017

---
+++ backend.tf
@@ -0,0 +1,16 @@
+# =============================================================================
+# BACKEND CONFIGURATION - CI/CD STANDALONE
+# =============================================================================
+
+terraform {
+  # Backend local - estado en terraform.tfstate
+  # Para usar S3 backend, descomenta y configura:
+  
+  backend "s3" {
+    bucket         = "cicd-iac-terraform-state-dev-dam2ffrv"
+    key            =  "dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+  
+}
